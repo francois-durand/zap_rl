4b26666554556292870244c7c3b1e223ca34012e
francois-durand <fradurandpub@gmail.com>
2021-04-30 09:26:18 +0000

    Deploying to gh-pages from  @ 0ee040e2b92a27d274dfec1589503703cd24c284 🚀

--- lib/zap_rl/__init__.py
+++ lib/zap_rl/__init__.py
@@ -5,6 +5,4 @@ __email__ = 'fradurand@gmail.com'
 __version__ = '0.1.0'
 
 
-from zap_rl.sub_package_1.my_class_1 import MyClass1
-from zap_rl.sub_package_2.my_class_2 import MyClass2
-from zap_rl.sub_package_2.my_class_3 import MyClass3
+from zap_rl.update_pseudo_inverse import update_pseudo_inverse
